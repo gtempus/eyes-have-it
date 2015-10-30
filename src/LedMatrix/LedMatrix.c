e8aee02fe4bc5c10e37556fb95aa4836b5ad575b
Gary Tempus, Jr <gtempus@codegaloot.com>
2015-10-30 21:35:43 +0000

    ping-pong dem eyes

--- src/LedMatrix/LedMatrix.c
+++ src/LedMatrix/LedMatrix.c
@@ -50,6 +50,178 @@ void splash_octocat() {
     _delay_ms(3000);        
 }
 
+void bullseye() {
+    write_data(0x01, 0b00111100); _delay_ms(70);
+    write_data(0x02, 0b01000010); _delay_ms(70);
+    write_data(0x03, 0b10011001); _delay_ms(70);
+    write_data(0x04, 0b10100101); _delay_ms(70);
+    write_data(0x05, 0b10100101); _delay_ms(70);
+    write_data(0x06, 0b10011001); _delay_ms(70);
+    write_data(0x07, 0b01000010); _delay_ms(70);
+    write_data(0x08, 0b00111100); _delay_ms(3000);
+    clear();
+}
+
+void ping_pong()
+{
+    single_data(0x01, 0b00000000, 0x01, 0b00000000); 
+    single_data(0x02, 0b00000000, 0x02, 0b00000000); 
+    single_data(0x03, 0b00011000, 0x03, 0b00011000); 
+    single_data(0x04, 0b00100100, 0x04, 0b00100100); 
+    single_data(0x05, 0b00100100, 0x05, 0b00100100); 
+    single_data(0x06, 0b00011000, 0x06, 0b00011000); 
+    single_data(0x07, 0b00000000, 0x07, 0b00000000); 
+    single_data(0x08, 0b00000000, 0x08, 0b00000000); _delay_ms(35);
+    
+    single_data(0x03, 0b00001100, 0x03, 0b00110000); 
+    single_data(0x04, 0b00010010, 0x04, 0b01001000); 
+    single_data(0x05, 0b00010010, 0x05, 0b01001000); 
+    single_data(0x06, 0b00001100, 0x06, 0b00110000); _delay_ms(35);
+    
+    single_data(0x03, 0b00000110, 0x03, 0b01100000); 
+    single_data(0x04, 0b00001001, 0x04, 0b10010000); 
+    single_data(0x05, 0b00001001, 0x05, 0b10010000); 
+    single_data(0x06, 0b00000110, 0x06, 0b01100000); _delay_ms(35);
+
+    for (int i=0; i<3; i++) {
+    single_data(0x03, 0b00000011, 0x03, 0b11000000); 
+    single_data(0x04, 0b00000100, 0x04, 0b00100000); 
+    single_data(0x05, 0b00000100, 0x05, 0b00100000); 
+    single_data(0x06, 0b00000011, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b01100000); 
+    single_data(0x04, 0b00000100, 0x04, 0b10010000); 
+    single_data(0x05, 0b00000100, 0x05, 0b10010000); 
+    single_data(0x06, 0b00000011, 0x06, 0b01100000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00110000); 
+    single_data(0x04, 0b00000100, 0x04, 0b01001000); 
+    single_data(0x05, 0b00000100, 0x05, 0b01001000); 
+    single_data(0x06, 0b00000011, 0x06, 0b00110000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00011000); 
+    single_data(0x04, 0b00000100, 0x04, 0b00100100); 
+    single_data(0x05, 0b00000100, 0x05, 0b00100100); 
+    single_data(0x06, 0b00000011, 0x06, 0b00011000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00001100); 
+    single_data(0x04, 0b00000100, 0x04, 0b00010010); 
+    single_data(0x05, 0b00000100, 0x05, 0b00010010); 
+    single_data(0x06, 0b00000011, 0x06, 0b00001100); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00000110); 
+    single_data(0x04, 0b00000100, 0x04, 0b00001001); 
+    single_data(0x05, 0b00000100, 0x05, 0b00001001); 
+    single_data(0x06, 0b00000011, 0x06, 0b00000110); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00000011); 
+    single_data(0x04, 0b00000100, 0x04, 0b00000100); 
+    single_data(0x05, 0b00000100, 0x05, 0b00000100); 
+    single_data(0x06, 0b00000011, 0x06, 0b00000011); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00000110); 
+    single_data(0x04, 0b00000100, 0x04, 0b00001001); 
+    single_data(0x05, 0b00000100, 0x05, 0b00001001); 
+    single_data(0x06, 0b00000011, 0x06, 0b00000110); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00001100); 
+    single_data(0x04, 0b00000100, 0x04, 0b00010010); 
+    single_data(0x05, 0b00000100, 0x05, 0b00010010); 
+    single_data(0x06, 0b00000011, 0x06, 0b00001100); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00011000); 
+    single_data(0x04, 0b00000100, 0x04, 0b00100100); 
+    single_data(0x05, 0b00000100, 0x05, 0b00100100); 
+    single_data(0x06, 0b00000011, 0x06, 0b00011000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b00110000); 
+    single_data(0x04, 0b00000100, 0x04, 0b01001000); 
+    single_data(0x05, 0b00000100, 0x05, 0b01001000); 
+    single_data(0x06, 0b00000011, 0x06, 0b00110000); _delay_ms(35);
+
+    single_data(0x03, 0b00000011, 0x03, 0b01100000); 
+    single_data(0x04, 0b00000100, 0x04, 0b10010000); 
+    single_data(0x05, 0b00000100, 0x05, 0b10010000); 
+    single_data(0x06, 0b00000011, 0x06, 0b01100000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000011, 0x03, 0b11000000); 
+    single_data(0x04, 0b00000100, 0x04, 0b00100000); 
+    single_data(0x05, 0b00000100, 0x05, 0b00100000); 
+    single_data(0x06, 0b00000011, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000110, 0x03, 0b11000000); 
+    single_data(0x04, 0b00001001, 0x04, 0b00100000); 
+    single_data(0x05, 0b00001001, 0x05, 0b00100000); 
+    single_data(0x06, 0b00000110, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00001100, 0x03, 0b11000000); 
+    single_data(0x04, 0b00010010, 0x04, 0b00100000); 
+    single_data(0x05, 0b00010010, 0x05, 0b00100000); 
+    single_data(0x06, 0b00001100, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00011000, 0x03, 0b11000000); 
+    single_data(0x04, 0b00100100, 0x04, 0b00100000); 
+    single_data(0x05, 0b00100100, 0x05, 0b00100000); 
+    single_data(0x06, 0b00011000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00110000, 0x03, 0b11000000); 
+    single_data(0x04, 0b01001000, 0x04, 0b00100000); 
+    single_data(0x05, 0b01001000, 0x05, 0b00100000); 
+    single_data(0x06, 0b00110000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b01100000, 0x03, 0b11000000); 
+    single_data(0x04, 0b10010000, 0x04, 0b00100000); 
+    single_data(0x05, 0b10010000, 0x05, 0b00100000); 
+    single_data(0x06, 0b01100000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b11000000, 0x03, 0b11000000); 
+    single_data(0x04, 0b00100000, 0x04, 0b00100000); 
+    single_data(0x05, 0b00100000, 0x05, 0b00100000); 
+    single_data(0x06, 0b11000000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b01100000, 0x03, 0b11000000); 
+    single_data(0x04, 0b10010000, 0x04, 0b00100000); 
+    single_data(0x05, 0b10010000, 0x05, 0b00100000); 
+    single_data(0x06, 0b01100000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00110000, 0x03, 0b11000000); 
+    single_data(0x04, 0b01001000, 0x04, 0b00100000); 
+    single_data(0x05, 0b01001000, 0x05, 0b00100000); 
+    single_data(0x06, 0b00110000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00011000, 0x03, 0b11000000); 
+    single_data(0x04, 0b00100100, 0x04, 0b00100000); 
+    single_data(0x05, 0b00100100, 0x05, 0b00100000); 
+    single_data(0x06, 0b00011000, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00001100, 0x03, 0b11000000); 
+    single_data(0x04, 0b00010010, 0x04, 0b00100000); 
+    single_data(0x05, 0b00010010, 0x05, 0b00100000); 
+    single_data(0x06, 0b00001100, 0x06, 0b11000000); _delay_ms(35);
+     
+    single_data(0x03, 0b00000110, 0x03, 0b11000000); 
+    single_data(0x04, 0b00001001, 0x04, 0b00100000); 
+    single_data(0x05, 0b00001001, 0x05, 0b00100000); 
+    single_data(0x06, 0b00000110, 0x06, 0b11000000); _delay_ms(35);
+     
+    }
+
+    single_data(0x03, 0b00000110, 0x03, 0b01100000); 
+    single_data(0x04, 0b00001001, 0x04, 0b10010000); 
+    single_data(0x05, 0b00001001, 0x05, 0b10010000); 
+    single_data(0x06, 0b00000110, 0x06, 0b01100000); _delay_ms(35);
+
+    single_data(0x03, 0b00001100, 0x03, 0b00110000); 
+    single_data(0x04, 0b00010010, 0x04, 0b01001000); 
+    single_data(0x05, 0b00010010, 0x05, 0b01001000); 
+    single_data(0x06, 0b00001100, 0x06, 0b00110000); _delay_ms(35);
+    
+    single_data(0x03, 0b00011000, 0x03, 0b00011000); 
+    single_data(0x04, 0b00100100, 0x04, 0b00100100); 
+    single_data(0x05, 0b00100100, 0x05, 0b00100100); 
+    single_data(0x06, 0b00011000, 0x06, 0b00011000); 
+ }
+
 void clear() {
     write_data(0x01, 0b00000000);
     write_data(0x02, 0b00000000);
@@ -59,6 +231,43 @@ void clear() {
     write_data(0x06, 0b00000000);
     write_data(0x07, 0b00000000);
     write_data(0x08, 0b00000000);
+}
+
+void emoji() {
+        clear(); _delay_ms(3000);
+        
+        write_data(0x0A, 0b00011000);    
+        write_data(0x03, 0b00011000); _delay_ms(70);
+        write_data(0x04, 0b00100100); _delay_ms(70);
+        write_data(0x05, 0b01000010); _delay_ms(3000);
+    
+        write_data(0x03, 0b00000000); _delay_ms(70);
+        write_data(0x04, 0b00111100); _delay_ms(200);
+           
+        write_data(0x04, 0b00000000); _delay_ms(70);
+        write_data(0x05, 0b01111110); _delay_ms(1500);
+           
+        write_data(0x04, 0b00111100); _delay_ms(70);
+        write_data(0x05, 0b01000010); _delay_ms(200); 
+
+        write_data(0x03, 0b00011000); _delay_ms(70);
+        write_data(0x04, 0b00100100); _delay_ms(70);
+        write_data(0x05, 0b01000010); _delay_ms(3000); 
+
+        write_data(0x03, 0b00000000); _delay_ms(70);
+        write_data(0x04, 0b00111100); _delay_ms(200); 
+
+        write_data(0x04, 0b00000000); _delay_ms(70);
+        write_data(0x05, 0b01111110); _delay_ms(200);
+        
+        write_data(0x04, 0b00111100); _delay_ms(70);
+        write_data(0x05, 0b01000010); _delay_ms(200); 
+
+        write_data(0x03, 0b00011000); _delay_ms(70);
+        write_data(0x04, 0b00100100); _delay_ms(70);
+        write_data(0x05, 0b01000010); _delay_ms(3000); 
+
+        clear(); _delay_ms(3000);
 }
 
 int main(void) {
@@ -83,9 +292,15 @@ int main(void) {
 
     while(1) {
         clear(); _delay_ms(3000);
-        
         write_data(0x0A, 0b00000011);    
+
+        ping_pong();
+        
+        bullseye();
+        
         write_data(0x08, 0b00011000); _delay_ms(100);
+
+        emoji();
 
         write_data(0x0A, 0b00000110);    
         write_data(0x07, 0b00011000);
