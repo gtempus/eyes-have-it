2bd99c45e43d44abd2c65e00a791dc9bfa3b0782
Gary Tempus, Jr <gtempus@codegaloot.com>
2015-10-30 03:46:30 +0000

    Play with the intensity register.

--- src/LedMatrix/LedMatrix.c
+++ src/LedMatrix/LedMatrix.c
@@ -74,7 +74,7 @@ int main(void) {
     SPCR |= (1 << SPE);
 
     write_data(0x0B, 0b00000111);
-    write_data(0x0A, 0b00001110);
+    write_data(0x0A, 0b00001111);
     write_data(0x0F, 0b00000000);
     write_data(0x0C, 0b00000001);
 
@@ -82,22 +82,27 @@ int main(void) {
     splash_octocat();
 
     while(1) {
-        clear();
-            
-        write_data(0x08, 0b00011000); _delay_ms(100);
+        clear(); _delay_ms(3000);
         
+        write_data(0x0A, 0b00000011);    
+        write_data(0x08, 0b00011000); _delay_ms(100);
+
+        write_data(0x0A, 0b00000110);    
         write_data(0x07, 0b00011000);
         write_data(0x08, 0b00100100); _delay_ms(100);
-        
+
+        write_data(0x0A, 0b00001001);            
         write_data(0x06, 0b00011000);
         write_data(0x07, 0b00100100);
         write_data(0x08, 0b00100100); _delay_ms(100);
-        
+
+        write_data(0x0A, 0b00001100);            
         write_data(0x05, 0b00011000);
         write_data(0x06, 0b00100100);
         write_data(0x07, 0b00100100);
         write_data(0x08, 0b00011000); _delay_ms(100);
-        
+
+        write_data(0x0A, 0b00001111);            
         write_data(0x04, 0b00011000);
         write_data(0x05, 0b00100100);
         write_data(0x06, 0b00100100);
@@ -155,6 +160,11 @@ int main(void) {
         single_data(0x01, 0b11000000, 0x01, 0b00000011);
         single_data(0x02, 0b00111100, 0x02, 0b00111100);
         single_data(0x03, 0b00000011, 0x03, 0b11000000); _delay_ms(3000);
+
+        for(char i = 15; i > 0; i--)
+        {
+            write_data(0x0A,i); _delay_ms(30);
+        }
         
     }
 
